1c9578df85747cb562dea102815b77bd8c70cbc4
DamianBajofer <damian.d.bajofer@gmail.com>
2022-06-02 20:12:12 -0300

    Actualizacion Puerto

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
 
+app.set("port", process.env.PORT || 30000)
+
 app.get("/", (request, response) => {
     response.send("HOLA PERROS")
 })
-app.listen(3000, () => {
+app.listen(app.get("port"), () => {
     console.log("HOLA PERROS!")
 })
