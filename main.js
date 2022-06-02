f05835d6533177c9b16e644dc374fe2c0ff19980
DamianBajofer <damian.d.bajofer@gmail.com>
2022-06-02 20:47:52 -0300

    test

--- main.js
+++ main.js
@@ -3,9 +3,5 @@ const app = express()
 
 // app.set("port", process.env.PORT || 30000)
 
-app.get("/", (request, response) => {
-    response.send("HOLA PERROS")
-})
-
 const port = process.env.PORT || 3000
 app.listen(port)
