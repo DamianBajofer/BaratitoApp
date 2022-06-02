276affcc7df945205b6312d22b5b6dec9378b66c
DamianBajofer <damian.d.bajofer@gmail.com>
2022-06-02 11:07:39 -0300

    Simple Server Start

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (request, response) => {
+    response.send("HOLA PERROS")
+})
+
+app.listen(3000, () => {
+    console.log("HOLA PERROS!")
+})
