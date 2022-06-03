3df12ae179e1de2c3202f34c8e2b9aeeda0c8780
DamianBajofer <damian.d.bajofer@gmail.com>
2022-06-03 00:51:48 -0300

    Prueba Vistas 10000%

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ app.get("/", (request, response) => {
 })
 
 app.get("/home", (request, response) => {
-    response.render(path.join(__dirname, "views", "home.html"))
+    response.render(path.join(`${__dirname}/views/home.html`))
 })
 
 app.get("/register", (request, response) => {
-    response.render(path.join(__dirname, "views", "register"))
+    response.render(path.join(path.join(`${__dirname}/views/register`))
 })
 
 app.get("/login", (request, response) => {
