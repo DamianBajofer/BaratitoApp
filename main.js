bb790e26eeef92bcc130db51dbc3bfc94bf0eb05
DamianBajofer <damian.d.bajofer@gmail.com>
2022-06-03 00:03:04 -0300

    test

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = express()
 
 app.set("port", process.env.PORT || 3000)
 
-app.get("/", (request, response) => {
-    response.render("index")
+app.get("/home", (request, response) => {
+    response.render("./src/views/index")
 })
 app.listen(app.get("port"), () => {
     console.log(`Escuchando en puerto ${process.env.PORT}`)
