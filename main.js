4da2e1d4a5e8e0632162e2360400e0a39a6fa9b0
DamianBajofer <damian.d.bajofer@gmail.com>
2022-06-02 21:43:56 -0300

    pruebas

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
 
-// app.set("port", process.env.PORT || 30000)
+app.set("port", process.env.PORT || 3000)
 
-const port = process.env.PORT || 3000
-app.listen(port)
+app.get("/", (request, response) => {
+    response.send("HOLA PERROS")
+})
+app.listen(app.get("port"), () => {
+    console.log(`Escuchando en puerto ${process.env.PORT}`)
+})
